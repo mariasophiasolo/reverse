59f597c447a036eb2885a23d06ac67830d0a27d3
Maria Sophia M. Solo <solomariasophia@gmail.com>
2024-01-27 21:47:49 +0800

    trial

---
+++ reverse.py
@@ -0,0 +1,13 @@
+# ask user to input their year of birth then reverse it
+
+def reverse_digits(year):
+    # convert the user input to a string and reverse it
+    print("Year of Birth:", year)
+    reverse_digits = ''.join(str(year)[::-1])
+
+    # print now the reversed digit with spaces in between
+    print("Reverse digits: ", reverse_digits)
+
+user_input = int(input("Enter your year of birth: "))
+reverse_digits(user_input)
+
